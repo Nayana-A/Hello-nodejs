6dd067bd2b29834e57ca7c2cd62e745f23547b8e
Nayana A <NayanaPOC@NainaAshok.onmicrosoft.com>
2025-10-24 11:27:04 +0530

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from AKS! 🚀');
+  res.send('Hello from AKS!This is Nayana 🚀');
 });
 
 app.listen(port, () => {
